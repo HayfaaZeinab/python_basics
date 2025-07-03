af4897c63540006df7abc090941d5fec3d5306df
HayfaaZeinab <23f1001503@ds.study.iitm.ac.in>
2025-07-03 19:11:12 +0530

    Add files via upload

---
+++ Day-01/intro.py
@@ -0,0 +1,4 @@
+a=input("Enter your name : ")
+b=input("Enter your age : ")
+c=input("Enter your hobby : ")
+print(f"hello!! I'm {a},{b} years old and i love {c}.")
---
+++ Day-01/nameage.py
@@ -0,0 +1,3 @@
+a=input("Enter you name : ")
+b=input("Enter your age : ")
+print(f"{a} is {b} years old.")
