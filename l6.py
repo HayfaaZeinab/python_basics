1579cdb5806da6199b96373dfa405e6ab9c6f036
HayfaaZeinab <23f1001503@ds.study.iitm.ac.in>
2025-11-03 23:52:35 +0530

    Add plusOne function to Solution class

--- l6.py
+++ l6.py
@@ -6,4 +6,15 @@ class Solution(object):
         a=s.split()
         return len(a[-1])
 
-  
+#plus one
+
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+
+        return [1] + digits
